7936e03615bdf696a8b5d7054a585ff82d5651fa
Ofirroami <ofir913001@gmail.com>
2025-07-18 12:16:52 +0300

    Creat new closure for post likes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 alert ("hello")
+
+function scoping(){
+    let likes = 0;
+
+    //closure
+    return function(){
+        likes++;
+    }
+}
+
+let likespost1 = scoping("Post Fight")
+likespost1();
+likespost1();
